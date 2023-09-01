68fb3d64e6567aee05a129e6e8ad0bc960ecc696
Tung Tran <tunganhtran94@gmail.com>
2023-09-01 10:50:10 +0700

    feat: multi process

---
+++ multiprocess/main.py
@@ -0,0 +1,24 @@
+"""Example of using multiprocessing module."""
+
+from multiprocessing import Process
+import os
+
+
+def worker(identifier):
+    """Worker."""
+    print(f"Worker {identifier} starting, pid: {os.getpid()}")
+
+
+def main():
+    """Start worker processes."""
+    processes = []
+    for i in range(5):
+        process = Process(target=worker, args=(i,))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+
+if __name__ == "__main__":
+    main()
